afcfec6d4af302f1d9f0a153438ee1abbf2a0a07
beerpsi <lacvtg.a1.2023@gmail.com>
2022-01-07 17:31:24 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,135 @@
+Current API: `https://beerpsi.me/api/v1/`
+
+# API
+A simple API intended for looking up bypass + app combinations. It currently has two public endpoints:
+
+`GET /app`: List all apps known to have jailbreak detection
+```json
+{
+    "status": "Successful",
+    "data": [
+        "8 Ball Pool",
+        "AASTOCKS M+ Mobile",
+        "Abamobi com",
+        "ABC News",
+        "Absher",
+        "Acuralink",
+        "ADIB Mobile Banking App",
+        "AFL Live Official App",
+        "AIB Mobile",
+        "ALEXBANK Mobile Banking",
+        "ALHOSN UAE",
+        "Alipay"
+    ]
+}
+```
+
+`GET /app?search=<app name>`: Lookup the app with the keyword `<app name>`  
+**Request:** `/app?search=balls`
+```json
+{
+    "status": "Successful",
+    "data": [
+        {
+            "name": "8 Ball Pool",
+            "uri": "https://apps.apple.com/us/app/8-ball-pool/id543186831",
+            "bypasses": [
+                {
+                    "name": "Liberty Lite (Beta)",
+                    "guide": "https://bypass.beerpsi.me/#/tools/tweaks?id=liberty-lite-beta",
+                    "repository": {
+                        "uri":"https://ryleyangus.com/repo"
+                    }
+                }
+            ]
+        }
+    ]
+}
+```
+
+`POST /gh-webhook`:
+**Requires the API to be set up as a systemd service, see below**
+
+As the name implies, this is where the GitHub webhook goes. When an appropriate `POST` request is sent, the database is refreshed.  
+To prevent anyone from willy-nilly sending a POST request and refreshing the database, this endpoint is only active if the environment variable `GITHUB_WEBHOOK_SECRET` is set.
+
+
+# Running the API yourself
+Quickly set up a local dev server for testing changes:
+```bash
+# *nix
+python -m venv env/ && source env/bin/activate
+pip install -r openapi/requirements.txt
+python3 openapi/api.py
+```
+
+```powershell
+# Windows
+python -m venv env/ && env\Scripts\Activate.ps1 # powershell
+pip install -r openapi/requirements.txt
+python3 openapi/api.py
+
+# If Flask is complaining, try restarting the Host Networking Service
+net stop hns && net start hns
+```
+
+
+# Making the API public
+Here's how `https://beerpsi.me/api/v1` was set up, but depending on your webserver and stuff you can do it differently.
+
+The API uses uWSGI, so you should install that:
+```bash
+pip install uwsgi
+```
+
+Then, create a file called `app.ini` that should ideally be under `openapi/`:
+```ini
+[uwsgi]
+module = api:app
+
+master = true
+processes = 5
+
+socket = api.sock
+chmod-socket = 660
+vacuum = true
+
+die-on-term = true
+```
+
+Create a systemd service (change `/var/www/jbdetectlist` to where you put the API):
+```ini
+[Unit]
+Description='An API for querying jailbreak bypasses'
+After=network.target
+
+[Service]
+Environment=GITHUB_WEBHOOK_SECRET=<This is for webhooking>
+User=www-data
+Group=root
+WorkingDirectory=/var/www/jbdetectlist/openapi
+ExecStart=/var/www/jbdetectlist/env/bin/uwsgi --ini api.ini
+
+[Install]
+WantedBy=multi-user.target
+```
+
+Allow the user running the API to restart the service without a password by creating `/etc/sudoers.d/auto-deploy` (important for auto-deploy):
+```
+Cmnd_Alias MYAPP_CMNDS = /bin/systemctl start jbdetectapi, /bin/systemctl stop jbdetectapi, /bin/systemctl restart jbdetectapi
+www-data ALL=(ALL) NOPASSWD: MYAPP_CMNDS
+```
+
+Start and enable the service, then configure nginx:
+```nginx
+server {
+    server_name beerpsi.me;
+    location /api/v1 {
+        rewrite ^/api/v1/(.*) /$1 break;
+        include uwsgi_params;
+        uwsgi_pass unix:/var/www/jbdetectlist/openapi/api.sock;
+    }
+}
+```
+
+and that's about it!
---
+++ api.py
@@ -0,0 +1,137 @@
+import os
+import yaml
+import hmac
+import hashlib
+
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+
+from fuzzywuzzy import fuzz
+
+
+def return_results(list_of_dicts, query, threshold):
+    query = query.lower()
+    scores = list()
+    for index, item in enumerate(list_of_dicts):
+        values = [item['name'].lower(), item['bundleId'].lower()]
+        ratios, partial_ratios = [fuzz.ratio(str(query), str(value)) for value in values], [fuzz.partial_ratio(str(query), str(value)) for value in values] # ensure both are in string
+        scores.append({"index": index, "partial_score": max(partial_ratios), "score": max(ratios)})
+
+    filtered_scores = [item for item in scores if item['score'] >= threshold or item['partial_score'] >= threshold]
+    sorted_filtered_scores = sorted(filtered_scores, key = lambda k: (k['score'], k['partial_score']), reverse=True)
+    if len(sorted_filtered_scores) > 0 and sorted_filtered_scores[0]['score'] == 100:
+        return [list_of_dicts[sorted_filtered_scores[0]['index']]]
+    filtered_list_of_dicts = [list_of_dicts[item["index"]] for item in sorted_filtered_scores]
+    return filtered_list_of_dicts
+
+
+def init_db(manifests_dir):
+    __scriptdir = os.path.dirname(os.path.realpath(__file__))
+    bypasses_file = os.path.join(__scriptdir, manifests_dir, 'bypasses.yaml')
+    apps_dir = os.path.join(__scriptdir, manifests_dir, 'apps')
+
+    with open(bypasses_file, encoding='utf-8') as file:
+        bypasses = yaml.safe_load(file)
+
+    db_data = list()
+    apps_files = [os.path.join(apps_dir, f) for f in os.listdir(apps_dir) if os.path.isfile(os.path.join(apps_dir, f)) and os.path.splitext(f)[-1].lower() == '.yaml']
+    for app_file in apps_files:
+        with open(app_file, encoding='utf-8') as file:
+            app = yaml.safe_load(file.read())
+        db_data.append(app)
+    apps = [x['name'] for x in db_data]
+    apps.sort(key=lambda a: a.lower())
+    return bypasses, apps, db_data
+
+
+def markdown_link(name, uri, sharerepo=False):
+    sharerepo_site = "https://sharerepo.stkc.win/?repo="
+    return f"[{name}]({sharerepo_site}{uri})" if sharerepo else f"[{name}]({uri})"
+
+class App(Resource):
+    def __init__(self):
+        self.bypasses, self.apps, self.db = init_db(os.path.join('manifests'))
+
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('search', required=False)
+        parser.add_argument('fields', required=False)
+
+        args = parser.parse_args()
+        if args.search is None:
+            return {'status': 'Successful', 'data': self.apps}
+        else:
+            search_results = return_results(self.db, args.search, 90)
+
+            for index, res in enumerate(search_results):
+                if res['bypasses']:
+                    bypass_notes = list()
+                    detailed_bypass_info = list()
+                    downgrade_noted = False
+                    for bypass in res['bypasses']:
+                        if 'name' in bypass:
+                            notes_from_bypass = self.bypasses[bypass['name']]['notes'] \
+                                                if 'notes' in self.bypasses[bypass['name']] \
+                                                else None
+                            if 'guide' in self.bypasses[bypass['name']]:
+                                bypass['guide'] = self.bypasses[bypass['name']]['guide']
+
+                            if 'repository' in self.bypasses[bypass['name']]:
+                                bypass['repository'] = self.bypasses[bypass['name']]['repository']
+                                bypass['repository']['uri'] = f"https://sharerepo.stkc.win/?repo={self.bypasses[bypass['name']]['repository']['uri']}"
+                            else:
+                                bypass['repository'] = None
+
+                            if not downgrade_noted and 'version' in bypass and bypass['name'] != "AppStore++":
+                                bypass_notes.append(
+                                    f"Use AppStore++ ({markdown_link('repo', self.bypasses['AppStore++']['repository']['uri'], sharerepo=True)}) to downgrade.")
+                                downgrade_noted = True
+
+                            notes_from_bypass = f"{self.bypasses[bypass['name']]['notes']}" \
+                                                    if 'notes' in self.bypasses[bypass['name']] \
+                                                    else None
+                            notes_from_manifest = bypass['notes'] \
+                                                    if 'notes' in bypass \
+                                                    else None
+                            if notes_from_bypass or notes_from_manifest:
+                                bypass_notes.append(' '.join(filter(None, [notes_from_bypass, notes_from_manifest])))
+                            if bypass_notes:
+                                bypass['notes'] = '\n'.join(bypass_notes)
+
+                        detailed_bypass_info.append(bypass)
+                    search_results[index]['bypasses'] = detailed_bypass_info
+
+            if search_results:
+                return {'status': 'Successful', 'data': search_results}
+            else:
+                return {'status': 'Not Found'}
+
+
+class GitHubWebhook(Resource):
+    def __init__(self):
+        self.webhook_secret = os.environ.get('GITHUB_WEBHOOK_SECRET').encode('utf-8')
+
+    def post(self):
+        signature = 'sha256=' + hmac.new(self.webhook_secret, request.data, hashlib.sha256).hexdigest()
+        if hmac.compare_digest(signature, request.headers.get('X-Hub-Signature-256')):
+            content = request.json
+            if content['ref'] == 'refs/heads/main':
+                systemd_service = 'jbdetectapi'
+                os.system(f'git pull')
+                os.system(f'sudo /bin/systemctl restart {systemd_service}')
+        else:
+            return "Signatures didn't match!", 500
+
+
+app = Flask(__name__)
+api = Api(app)
+api.add_resource(App, '/app')
+if 'GITHUB_WEBHOOK_SECRET' in os.environ:
+    api.add_resource(GitHubWebhook, '/gh-webhook')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
